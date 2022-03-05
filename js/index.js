3ca716c2f003a4fb9c881044246578382fdfcb2d
Elemes <elemes@umov.me>
2022-03-05 05:33:10 -0300

    done

---
+++ js/index.js
@@ -0,0 +1,47 @@
+const cards = [
+    { name: 'aquaman', img: 'aquaman.jpg' },
+    { name: 'batman', img: 'batman.jpg' },
+    { name: 'captain america', img: 'captain-america.jpg' },
+    { name: 'fantastic four', img: 'fantastic-four.jpg' },
+    { name: 'flash', img: 'flash.jpg' },
+    { name: 'green arrow', img: 'green-arrow.jpg' },
+    { name: 'green lantern', img: 'green-lantern.jpg' },
+    { name: 'ironman', img: 'ironman.jpg' },
+    { name: 'spiderman', img: 'spiderman.jpg' },
+    { name: 'superman', img: 'superman.jpg' },
+    { name: 'the avengers', img: 'the-avengers.jpg' },
+    { name: 'thor', img: 'thor.jpg' },
+    { name: 'aquaman', img: 'aquaman.jpg' },
+    { name: 'batman', img: 'batman.jpg' },
+    { name: 'captain america', img: 'captain-america.jpg' },
+    { name: 'fantastic four', img: 'fantastic-four.jpg' },
+    { name: 'flash', img: 'flash.jpg' },
+    { name: 'green arrow', img: 'green-arrow.jpg' },
+    { name: 'green lantern', img: 'green-lantern.jpg' },
+    { name: 'ironman', img: 'ironman.jpg' },
+    { name: 'spiderman', img: 'spiderman.jpg' },
+    { name: 'superman', img: 'superman.jpg' },
+    { name: 'the avengers', img: 'the-avengers.jpg' },
+    { name: 'thor', img: 'thor.jpg' }
+  ];
+  
+  const memoryGame = new MemoryGame(cards);
+  
+  window.addEventListener('load', event => {
+    let html = '';
+    memoryGame.cards.forEach(pic => {
+      html += `<div class="card" data-card-name="${pic.name}">`;
+      html += `<div class="back" name="${pic.img}"></div>`;
+      html += `<div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`;
+      html += `</div>`;
+    });
+  
+    document.querySelector('#memory-board').innerHTML = html;
+  
+    document.querySelectorAll('.card').forEach(card => {
+      card.addEventListener('click', () => {
+        
+        console.log(`Card clicked: ${card}`);
+      });
+    });
+  });
---
+++ js/memoryGame.js
@@ -0,0 +1,50 @@
+class MemoryGame {
+    constructor(cards) {
+      this.cards = cards;
+      this.pickedCards = [];
+      this.pairsClicked = 0;
+      this.pairsGuessed = 0;
+    }
+  
+    shuffleCards(cards) {
+      if (!cards) {
+        return undefined;
+      }
+  
+      for (let i = cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = cards[i];
+        cards[i] = cards[j];
+        cards[j] = temp;
+      }
+      return cards;
+    }
+  
+    checkIfPair(card1, card2) {
+      this.pairsClicked += 1;
+  
+      if (card1 === card2) {
+        this.pairsGuessed += 1;
+        return true;
+      } else {
+        return false;
+      }
+  
+      function resetClickedPairs() {
+        this.pickedCards = [];
+      }
+  
+      resetClickedPairs();
+    }
+  
+    checkIfFinished() {
+      if (this.pairsGuessed === this.cards.length / 2) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+  
+  // The following is required for automated testing. Please, ignore it.
+  if (typeof module !== 'undefined') module.exports = MemoryGame;
